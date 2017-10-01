c6d068419411cb3e01c0413a82551b7d3e949561
BruunoMM <brunommarinho@hotmail.com>
2017-10-01 00:01:10 -0300

    Create dispersarArg.c

---
+++ dispersarArg.c
@@ -0,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "sort.h"
+
+int colocaCaixas(FILE *f, Item **itens, int nItens, int tamCaixa)
+{
+
+	//espaço restante numa caixa
+	int *restante;
+	int qtdCaixas = 0;
+	int i, e;
+	int tempo = time(NULL);
+	srand(tempo);
+
+	restante = (int*)malloc(nItens * sizeof(int));
+	if (!restante) {
+		printf("erro ao alocar restantes\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//Inicializando
+
+	for (i = 0;i<nItens;i++)
+		restante[i] = tamCaixa;
+
+	//Fim da inicializando
+
+	qtdCaixas = 1;
+
+	//dispersar
+	for (i = 0;i<nItens;i++) {
+		for (e = 0;e<qtdCaixas;e++) {
+			if (itens[i]->peso <= restante[e]) {
+				itens[i]->caixa = e;
+				restante[e] -= itens[i]->peso;
+				break;
+			}
+		}
+		if (itens[i]->caixa == -1) {
+			qtdCaixas++;
+			itens[i]->caixa = e;
+			restante[e] -= itens[i]->peso;
+		}
+	}
+
+	//Encontramos? uma solução! basta colocar num arquivo
+
+	//primeiro deixamos em ordem crescente de caixa;
+	sort(itens, nItens, comparaCaixaCrescente);
+
+	//então colocamos no arquivo
+
+	//qtdCaixas++;
+
+	fprintf(f, "%d\n", qtdCaixas);
+	for (i = 0, e = 0;i<nItens;i++) {
+
+		printf("posOrig:%d\tpeso:%d\tcaixa:%d\n", itens[i]->posOrig, itens[i]->peso, itens[i]->caixa);
+
+		if (itens[i]->caixa != e) fprintf(f, "\n");
+		fprintf(f, "%d\t", itens[i]->posOrig);
+		e = itens[i]->caixa;
+	}
+	fprintf(f, "\n");
+
+	printf("%d\n", tempo);
+
+	for (i = 0;i<qtdCaixas;i++) printf("restante[%d] = %d\n", i, restante[i]);
+
+	free(restante);
+	
+	return qtdCaixas;
+}
+
+int main(int argc, char **argv) {
+
+	int caixaSize;
+	int nItens, i;
+	int somaTudo = 0;
+	int minCaixas;
+	Item **itens;
+	FILE *f, *res;
+	clock_t tic, toc;
+	if (argc != 2) {
+		printf("Modo de uso: ./executavel path/nome_do_arquivo\n");
+		exit(EXIT_FAILURE);
+	}
+	tic = clock();
+
+	f = fopen(argv[1], "rt");
+	res = fopen("resultado/resultado.txt", "wt");
+	if (!f) {
+		printf("Erro ao abrir instancia.\n");
+		exit(EXIT_FAILURE);
+	}
+	fscanf(f, "%d\n", &nItens);
+	fscanf(f, "%d\n", &caixaSize);
+	itens = (Item**)malloc(nItens * sizeof(Item*));
+	
+	if (!itens) {
+		printf("erro ao alocar vetor itens\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//Inicializando
+
+	for (i = 0;i<nItens;i++) {
+		itens[i] = (Item*)malloc(sizeof(Item));
+		if (!itens[i]) {
+			printf("erro ao alocar item\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// Lendo do arquivo e preenchendo o vetor de ITENS.
+	for (i = 0;i<nItens;i++) {
+		fscanf(f, "%d\n", &(itens[i]->peso));
+		itens[i]->posOrig = i;
+		itens[i]->caixa = -1;
+	}
+	// Somando todos os pesos
+	for (i = 0;i<nItens;i++) {
+		somaTudo += itens[i]->peso;
+	}
+	minCaixas = somaTudo / caixaSize;
+	if (minCaixas*caixaSize<somaTudo) minCaixas++;
+	printf("%d\n", colocaCaixas(res, itens, nItens, caixaSize));
+
+	printf("soma:\n%d\n", somaTudo);
+	printf("tamanho:\n%d\n", caixaSize);
+	printf("caixas necessarias:\n%d\n", minCaixas);
+	for (i = 0;i<nItens;i++)
+		free(itens[i]);
+	free(itens);
+
+	toc = clock();
+	printf("Tempo de execucao: %f segundos\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+	return 0;
+}
